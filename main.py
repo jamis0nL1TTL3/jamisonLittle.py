b8e435a706c4089a42f2a52567c9c44e2e4c8e8c
kaabu <kbuch@cocc.edu>
2024-04-07 23:11:36 -0700

    git remote add origin https://github.com/Lchance2/LChance-Git-Assignment.git git branch -M main git push -u origin main

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 #Function 1
 # Returns Area of Rectangle
-def rect_area(width, length):
-    return width * length
 #Function 2
 # Returns Surface Area of Rectangular Solid
-def rect_surface_area(width, height, length):
-    #2 × base_area + lateral_area
-    return 2*(width*height + length*height)
-
 # Request the dimension of a solid rectangular object
 
 length = int(input("Enter the length of the the object as a integer: "))
