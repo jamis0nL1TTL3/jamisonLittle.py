2184ddef99c6ed6c70eed97cfa351ff2c28f8d74
ReKID-JaXIC <H.A.JamX099@gmail.com>
2025-04-02 11:55:18 -0700

    Update jacob_mead.py

--- jacob_mead.py
+++ jacob_mead.py
@@ -1,2 +1,7 @@
-def rect_surface_area(length: int, width: int, height: int, fun: function) -> int:
-    return 2 * (fun(legth, width) + fun(legth, height) + fun(width, height))
+def rect_surface_area(length: int, width: int, height: int) -> int:
+    return 2 * (rect_area(length, width) + rect_area(length, height) + rect_area(width, height))
+if __name__ == "__main__":
+    l = int(input("Length?\n>>"))
+    w = int(input("Width?\n>>"))
+    h = int(input("Height?\n>>"))
+    print(rect_surface_area(l, w, h))
