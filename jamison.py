6c5209f7b10e4e96b1cb1c9f91d0b92e716ad911
jamison GIThub <theorangecello@gmail.com>
2025-04-02 10:09:36 -0700

    Area function for a rectangle
    spring term first week assignment with partners

---
+++ jamison.py
@@ -0,0 +1,8 @@
+# area of a rectangle
+
+width = int(input("Please enter an integer for a rectangles width: "))
+length = int(input("Please enter an integer for a rectangles length: "))
+
+rect_area = width * length
+
+print (rect_area)
