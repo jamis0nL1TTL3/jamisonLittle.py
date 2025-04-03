f9f94b86db5042a2b2533118e9a983f7211be169
jamison GIThub <theorangecello@gmail.com>
2025-04-03 13:19:00 -0700

    updated area of a rectangle function (with doc strings)

--- jamison.py
+++ jamison.py
@@ -1,6 +1,19 @@
 # area of a rectangle using users input
 
 def rect_area():
+    '''Gets User input and uses given integers to calculate area of a rectangle
+
+    Parameters:
+    -----------
+    width: int
+        User inputs given width for the rectangle
+    length: int
+        User inputs given length for the rectangle
+
+    Returns:
+    --------
+    Multiplies the two given integers and prints out the result
+    '''
     width = int(input("Please enter an integer for the rectangle width: "))
     length = int(input("Please enter an integer for the rectangle length: "))
     return width * length
