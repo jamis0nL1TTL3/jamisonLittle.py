0d0a95c8242ee9ba0571c44426f0e02475be6ba4
jamison GIThub <theorangecello@gmail.com>
2025-04-04 15:49:04 -0700

    updated area of a rectangle function (with doc strings)(checked with jacob_mead code and ran throughout, result: clear and correct)

--- jamison.py
+++ jamison.py
@@ -1,4 +1,6 @@
-# area of a rectangle using users input
+# Make a function titled ‘rect_area’ that finds the area of a rectangle
+# Make a function titled ‘rect_surface_area’ that calls ‘rect_area’ to find the surface area of a rectangle
+# Ensure to print length, width, and height along with the total surface area at the end
 
 def rect_area(length:int, width:int) -> int:
     '''Multiplies length and width of a rectangle to find area
